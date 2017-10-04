faf8465669427ff1a15b1f59a2bc30424348889d
Uttakran Renuman <qowieury_kidmaioak@hotmail.com>
2017-10-04 16:44:05 +0700

    add INSERT part for store data

--- php_lumen/app/Http/Controllers/CustomerController.php
+++ php_lumen/app/Http/Controllers/CustomerController.php
@@ -1,11 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
+use App\Models\Customer;
 class CustomerController extends Controller
 {
     public function register()
     {
+
+        return view('register.index', [
+            'title' => 'Registration'
+        ]);
+    }
+    public function store_register_data(Request $request)
+    {
+        $name = $request->input('name');
+        $address = $request->input('address');
+        $password = crypt($request->input('password'), config('user.password.salt'));
+        //INSERT
+        $customer = new Customer;
+        $customer->name =$name;
+        $customer->address = $address;
+        $customer->password = $password;
+        $customer->save();
+        //end INSERT
         return view('register.index', [
             'title' => 'Registration'
         ]);
