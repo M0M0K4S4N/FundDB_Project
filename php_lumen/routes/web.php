5de237f3249b5551e8fdcddcfcd247119e79c62f
Uttakran Renuman <qowieury_kidmaioak@hotmail.com>
2017-10-04 16:40:48 +0700

    add route for store register data

--- php_lumen/routes/web.php
+++ php_lumen/routes/web.php
@@ -17,6 +17,7 @@ $router->get('/version', function () use ($router) {
 
 $router->get('/', 'IndexController@index');
 $router->get('/register', 'CustomerController@register');
+$router->post('/register', 'CustomerController@store_register_data');
 
 $router->get('/posts', 'PostController@index');
 $router->get('/posts/{id}', 'PostController@view');
