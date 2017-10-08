5b34b2b5d27097146c565eac0d4ed21346b6d17a
Uttakran Renuman <qowieury_kidmaioak@hotmail.com>
2017-10-08 14:46:33 +0700

    update required input

--- php_lumen/resources/views/register/index.blade.php
+++ php_lumen/resources/views/register/index.blade.php
@@ -5,15 +5,15 @@
 <div class="form-group">
   <label for="name">Name</label>
 
-  <input type="text" class="form-control" name="name"  placeholder="Name">
+  <input type="text" class="form-control" name="name"  placeholder="Name" required>
 </div>
 <div class="form-group">
   <label for="address">Address</label>
-  <textarea class="form-control" name="address" placeholder="Address" rows="3"></textarea>
+  <textarea class="form-control" name="address" placeholder="Address" rows="3" required></textarea>
 </div>
  <div class="form-group">
-  <label for="password">Password</label>
-  <input type="password" class="form-control" name="password"  placeholder="Password">
+  <label for="password" >Password</label>
+  <input type="password" class="form-control" name="password"  placeholder="Password" required>
 </div>
 <button type="submit" class="btn btn-primary">Register</button>
   <button type="reset" class="btn pull-right">Cancel</button>
