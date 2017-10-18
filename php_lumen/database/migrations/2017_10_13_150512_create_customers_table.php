d4ef97d56b6b696fa1bda04674aff87c5b00896c
Uttakran Renuman <qowieury_kidmaioak@hotmail.com>
2017-10-18 21:51:36 +0700

    add api_token prepare for auth

--- php_lumen/database/migrations/2017_10_13_150512_create_customers_table.php
+++ php_lumen/database/migrations/2017_10_13_150512_create_customers_table.php
@@ -19,6 +19,7 @@ class CreateCustomersTable extends Migration
             $table->string('name');
             $table->string('password');
             $table->string('address');
+            $table->string('api_token')->nullable();
         });
     }
 
