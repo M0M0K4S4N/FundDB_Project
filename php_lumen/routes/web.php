b38f191308fbde2253a25e73577ae581e704a534
Uttakran Renuman <qowieury_kidmaioak@hotmail.com>
2017-11-06 09:24:38 +0700

    add crud index

--- php_lumen/routes/web.php
+++ php_lumen/routes/web.php
@@ -30,6 +30,12 @@ $router->get('/customer/result', 'CustomerController@view_order_result');
 
 
 //crud in class
+$router->get('/crud', function(){
+  //echo 'aaa';
+  return view('crud.index', [
+      'title' => 'CRUD',
+    ]);
+});
 $router->get('/crud/register/view', 'CustomerController@view_register_data');
 $router->post('/crud/register/view/delete', 'CustomerController@delete_register_data');
 $router->get('/crud/order/view', 'CrudController@show_order_list');
